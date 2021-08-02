2f08ba1bc1795d961052fa4d43c92468ff1f7c3a
“Bintree” <395145114@qq.com>
2021-08-02 14:41:43 +0800

    修正至7.4

--- src/chineseInitial.php
+++ src/chineseInitial.php
@@ -14,17 +14,15 @@ class getInitial
 	 * @param string $targetKey 首字母的键名
 	 * @return array    根据首字母关联的二维数组
 	 */
-	public function groupByInitials(array $data, $targetKey = 'name')
-	{
-		$pinyins = $this->getPinyinList();
-
-		$data = array_map(function ($item) use ($targetKey,$pinyins) {
+	public function groupByInitials(array $data, string $targetKey = 'name'): array
+    {
+		$Pinyin = $this->getPinyinList();
+		$data = array_map(function ($item) use ($targetKey,$Pinyin) {
 			return array_merge($item, [
-				'initials' => $this->getInitials($item[$targetKey],$pinyins),
+				'initials' => $this->getInitials($item[$targetKey],$Pinyin),
 			]);
 		}, $data);
-		$data = $this->sortInitials($data);
-		return $data;
+        return $this->sortInitials($data);
 	}
 
 	/**
@@ -32,34 +30,41 @@ class getInitial
 	 * @param array $data
 	 * @return array
 	 */
-	public function sortInitials(array $data)
-	{
+	public function sortInitials(array $data): array
+    {
 		$sortData = [];
-		foreach ($data as $key => $value) {
+		foreach ($data as $value) {
 			$sortData[$value['initials']][] = $value;
 		}
 		ksort($sortData);
 		return $sortData;
 	}
 
+    /**
+     * 获取首字母
+     * @param string $str 汉字字符串
+     * @return string 首字母
+     */
+    public function getInitial(string $str): string
+    {
+        $str = trim($str);
+        $first_str= mb_convert_encoding($str,"UTF-8","GB2312");//转码
+        $fChar = ord($first_str[0]);
+        if ($fChar >= ord("A") and $fChar <= ord("z")) return strtoupper($first_str[0]);//英文首字母快键处理
+        $str = $this->unicode_encode($str);
+        return strtoupper($str[0]);
+    }
+
 	/**
-	 * 获取首字母
+	 * 批量获取首字母
 	 * @param string $str 汉字字符串
 	 * @return string 首字母
 	 */
-	public function getInitials($str,$pinyins)
-	{
-		$str = trim($str);
-
-		$first_str= mb_convert_encoding($str,"UTF-8","GB2312");//转码
-
-		$fchar = ord($first_str{0});
-		if ($fchar >= ord("A") and $fchar <= ord("z")) return strtoupper($first_str{0});//英文首字母快键处理
-		$str = $this->unicode_encode($str);
-
-		$str = strtoupper($str[0]);
-		if(isset($pinyins[$str])){
-			return $pinyins[$str];
+	public function getInitials(string $str, $Pinyin): string
+    {
+        $str = $this->getInitial($str);
+		if(isset($Pinyin[$str])){
+			return $Pinyin[$str];
 		}else{
 			return '#';
 		}
@@ -69,37 +74,36 @@ class getInitial
 	 * 获取拼音数组(可存缓存)
 	 * @return array 汉字数组
 	 */
-	public function getPinyinList()
-	{
+	public function getPinyinList(): array
+    {
+        $Pinyin = [];
 		$fp = fopen(dirname(__FILE__).'/pinyin.txt','r');
 		while(!feof($fp))
 		{
 			$line = trim(fgets($fp));
-			$zhengze = '/U\+(.*?): ([a-z]{1})/';
-			preg_match($zhengze,$line,$result);
-			$pinyins[$result[1]] = strtoupper($result[2]);
+			$rule = '/U\+(.*?): ([a-z])/';
+			preg_match($rule,$line,$result);
+            $Pinyin[$result[1]] = strtoupper($result[2]);
 		}
 		fclose($fp);
-		return $pinyins;
+		return $Pinyin;
 	}
 
 	/**
 	 * Unicode编码
 	 * @param string $str 字符串
-	 * @param string $encoding 编码类型
-	 * @param string $prefix 前缀
-	 * @param string $postfix 后缀
-	 * @return array 汉字数组
+     * @return array 汉字数组
 	 */
-	private function unicode_encode($str, $encoding = 'UTF-8', $prefix = '', $postfix = '') {
-		$str = iconv($encoding, 'UCS-2', $str);
-		$arrstr = str_split($str, 2);
-		$unistr = [];
-		for($i = 0, $len = count($arrstr); $i < $len; $i++) {
-			$dec = bin2hex($arrstr[$i]);
-			$unistr[] = $prefix . $dec . $postfix;
+	private function unicode_encode(string $str): array
+    {
+        $str = iconv('UTF-8', 'UCS-2', $str);
+		$arrStr = str_split($str, 2);
+		$unicodeStr = [];
+		for($i = 0, $len = count($arrStr); $i < $len; $i++) {
+			$dec = bin2hex($arrStr[$i]);
+            $unicodeStr[] = '' . $dec . '';
 		}
-		return $unistr;
+		return $unicodeStr;
 	}
 
 
